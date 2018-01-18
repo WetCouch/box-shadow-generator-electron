ee3286e9578b53a839a0006223b3e75a62dd7003
Wojciech Janoszek <wjanoszek@icloud.com>
2018-01-18 14:08:37 +0100

    feat: Connect sliders with CSS

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+function $ (id) {
+    return document.getElementById(id).value;
+}
+
 for (let i = 1; i <= 4; i++) {
     document.getElementById('slider'+i).oninput = function () {
         document.getElementById('output'+i).innerHTML = this.value;
+        document.getElementById('exampleBox').style.boxShadow = $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px black';
     }
 }
