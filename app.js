0b44c64ba8f73ef58a91bc11e4efe5907a701ab9
Wojciech Janoszek <wjanoszek@icloud.com>
2019-01-03 15:03:48 +0100

    feat: Return ready css-code

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ function $ (id) {
 for (let i = 1; i <= 4; i++) {
     document.getElementById('slider'+i).oninput = function () {
         document.getElementById('output'+i).value = this.value;
+        document.getElementById('styleCode').innerText = 'box-shadow: ' + $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px ' + $('shadowColor');
         document.getElementById('exampleBox').style.boxShadow = $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px ' + $('shadowColor');
     };
 
     document.getElementById('output'+i).oninput = function () {
         document.getElementById('slider'+i).value = this.value;
         document.getElementById('exampleBox').style.boxShadow = $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px ' + $('shadowColor');
+        document.getElementById('styleCode').innerText = 'box-shadow: ' + $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px ' + $('shadowColor');
     }
 
 }
 
 document.getElementById('shadowColor').oninput = function () {
     document.getElementById('exampleBox').style.boxShadow = $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px ' + $('shadowColor');
+    document.getElementById('styleCode').innerText = 'box-shadow: ' + $('slider1') + 'px ' + $('slider2') + 'px ' + $('slider3') + 'px ' + $('slider4') + 'px ' + $('shadowColor');
 };
